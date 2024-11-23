d02c0565cb053f44d1c23bb9761ce0ac31539dba
kalyandoki <kalyanreddy14343@gmail.com>
2024-11-23 13:06:17 +0530

    mn

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -13,9 +13,6 @@ export const createNote = async (note) => {
   }
 };
 
-// const API_BASE_URL =
-//   import.meta.env.REACT_APP_API_BASE_URL || "http://localhost:5000/notes";
-
 export const fetchNotes = async (query = {}) => {
   const { search = "", category = "" } = query;
   const response = await axios.get(API_BASE_URL, {
@@ -23,16 +20,6 @@ export const fetchNotes = async (query = {}) => {
   });
   return response.data;
 };
-
-// export const createNote = async (note) => {
-//   try {
-//     const response = await axios.post(API_BASE_URL, note);
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error creating note:", error);
-//     throw error;
-//   }
-// };
 
 export const updateNote = async (id, note) => {
   const response = await axios.put(`${API_BASE_URL}/${id}`, note);
