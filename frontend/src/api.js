7ff6a40c40a25ba0284ebaaa84001e2e09d2a368
kalyandoki <kalyanreddy14343@gmail.com>
2024-11-23 12:33:14 +0530

    api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/notes";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 console.log(import.meta.env.VITE_API_BASE_URL);
 
