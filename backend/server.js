1851ea6767611c0a802d2c7dede8ca2ccec24d35
kalyandoki <kalyanreddy14343@gmail.com>
2024-11-23 12:32:54 +0530

    ser

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,16 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const noteRoutes = require("./routes/noteRoutes");
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/notes", noteRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is working");
