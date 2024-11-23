c768cba5e82838342bd67de763acc6a54e0cef14
kalyandoki <kalyanreddy14343@gmail.com>
2024-11-23 12:58:54 +0530

    serjh

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -13,9 +13,5 @@ router.get("/", getNotes);
 router.post("/", createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
-router.post("/", (req, res, next) => {
-  console.log("POST /notes hit");
-  next();
-});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
-const cors = require("cors");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const cors = require("cors");
 const noteRoutes = require("./routes/noteRoutes");
 
-dotenv.config();
+const app = express();
 
-const PORT = process.env.PORT || 5000;
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/notes", noteRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is working");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Database Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.log("Database connection error:", error));
